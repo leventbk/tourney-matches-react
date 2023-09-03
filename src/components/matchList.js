944e008067ea8acf812ebe04550e402d4f2a44c3
Leventbk <leventbkarakus@hotmail.com>
2023-09-03 17:00:29 -0400

    update component functions

--- src/components/matchList.js
+++ src/components/matchList.js
@@ -1,4 +1,19 @@
-<section className="PlayerList MatchList">
-<h1>Match list</h1>
-{/* Matches will be shown here */}
-</section>
+import React from 'react';
+import Match from './Match';
+
+function MatchList() {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {MatchList.map((match) => (
+        <Match
+          key={match.id}
+          name={match.name}
+          wins={match.wins}
+        />
+      ))}
+    </section>
+  )
+}
+
+export default MatchList;
