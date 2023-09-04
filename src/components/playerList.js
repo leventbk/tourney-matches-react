c8ef26ef12ad2d500b0adab33006014af5b3905a
Leventbk <leventbkarakus@hotmail.com>
2023-09-04 19:17:17 -0400

    helpers funcs added to tested

--- src/components/playerList.js
+++ src/components/playerList.js
@@ -1,7 +1,16 @@
 import React from "react"; //optional
 import Player from "./Player";
 
+import matchData from "../data/matchData";
+import playerData from "../data/playerData";
+import {preparePlayerData, addWinsToPlayer} from "../helpers/playerHelpers"
+
+//PlayerList will need to create an array of Player data/components and output it.
 function PlayerList() {
+  const playerDataArray = preparePlayerData(playerData);
+  console.log(playerDataArray);
+  const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData); //this is the data we want to use to create the Player components
+  console.log(parsedPlayerData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
