0077f6ea39a4c551e68e3747f82c56b0ceb389a0
Leventbk <leventbkarakus@hotmail.com>
2023-09-08 17:37:11 -0400

    rendering all players

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,12 +1,26 @@
 import React from "react"; //optional
 import Player from "./Player";
+import playerData from "../data/playerData";
+import { preparePlayerData } from "../helpers/playerHelpers"
+
 
 function PlayerList(props) {
-  const onePlayer = props.playerData[0];
+
+  const allPlayers = preparePlayerData(playerData).map((player, gamerTag) => {
+    return (
+      <Player
+        key={player.gamerTag}
+        gamerTag={player.gamerTag}
+        firstName={player.firstName}
+        lastName={player.lastName}
+        wins = {player.wins}
+      />)
+  });
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
+      {allPlayers}
     </section>
   );
 }
