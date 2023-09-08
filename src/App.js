66d9a99ac59722d5e208de33e371f1ab0e41bc9e
Leventbk <leventbkarakus@hotmail.com>
2023-09-08 17:09:59 -0400

    Refactoring

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import React from 'react';
 import MatchList from './components/MatchList'; 
 import PlayerList from './components/PlayerList';
+import playerData from './data/playerData';
+import matchData from "./data/matchData";
+import {preparePlayerData, addWinsToPlayer} from "./helpers/playerHelpers"
 
 function App() {
+  
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData); //
+
   return (
     <div className="App">
       <h1>Tourney Matches <span>Where Coding and Tournaments Found their Match!</span></h1>
-      <PlayerList />
-      <MatchList />
+      <PlayerList playerData={parsedPlayerData} />
+      <MatchList matchData={matchData}/>
     </div>
   );
 }
--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,20 +1,12 @@
 import React from "react"; //optional
 import Player from "./Player";
 
-import matchData from "../data/matchData";
-import playerData from "../data/playerData";
-import {preparePlayerData, addWinsToPlayer} from "../helpers/playerHelpers"
-
-//PlayerList will need to create an array of Player data/components and output it.
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(playerData);
-  // console.log(playerDataArray);
-  const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData); //this is the data we want to use to create the Player components
-const onePlayer = parsedPlayerData[0];
+  const onePlayer = props.playerData[0];
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins}/>
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
 }
